13f1d0432d6ab2dfef6a5fd667cc97f518e62592
revanth <revanthreddy.mukundha@gmail.com>
2022-10-16 18:44:03 +0530

    practice

---
+++ JS1_Introduction/CRUD_Application/sample74jsclearjs.js
@@ -0,0 +1,12 @@
+function clearuser(){
+    document.getElementById("fname").value="";
+    document.getElementById("lname").value="";
+    document.getElementById("birthday").value="";
+    document.getElementById("birthday").value="";
+    document.getElementById("email").value="";
+    document.getElementById("phone").value="";
+
+    allradios.forEach((element)=>{
+       element.checked=false
+    })
+}
